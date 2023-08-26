04f125c2522cf35bf375112eee0a2e9317afd4bc
Jeffrey Jackson <main.jjackson@gmail.com>
2023-08-25 20:40:24 -0500

    add header and content replace

--- server.js
+++ server.js
@@ -1,14 +1,24 @@
 const fs = require('fs')
 const http = require('http')
 
-let html = fs.readFileSync('./index.html');
-
+let htmlob = {
+    index:fs.readFileSync('./index.html'),
+    home:fs.readFileSync('./home.html'),
+    contact:fs.readFileSync('./contact.html'),
+    about:fs.readFileSync('./about.html')
+}
+let html = fs.readFileSync('./basic.html','utf-8');
 const server =  http.createServer((req,res)=>{
     let path = req.url;
-    console.log('New Request Recieved')
-    res.end(path);
+    console.log('New Request Recieved '+path);
+
+    if(path === '/' || path.toLocaleLowerCase()=== '/index'){res.end(html.replace('{{%Content%}}','Index'))   
+    }else if(path.toLocaleLowerCase() === '/home'){res.end(html.replace('{{%Content%}}','Home'))
+    }else if(path.toLocaleLowerCase() === '/about'){res.end(html.replace('{{%Content%}}','About'))
+    }else if(path.toLocaleLowerCase() === '/contact'){res.end(html.replace('{{%Content%}}','Contact'))
+    }else{res.writeHead(404,{'content-type':'text/html'}).end();}
 })
 
-server.listen(3000,()=>{
+server.listen(3500,()=>{
     console.log("Server Open");
 })
